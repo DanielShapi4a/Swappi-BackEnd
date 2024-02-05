76e592213d8ad2bcc8620e6247f0ca68a3bffc97
SaharYehezkel <MrSaharYehezkel@gmail.com>
2024-02-05 19:08:52 +0200

    Register&Login in AuthController

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,16 +2,16 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 
-router.post('/create', async (req, res) => {
-  try {
-    const newUser = new User(req.query);
-    await newUser.save();
-    res.status(201).json({ message: 'User created successfully', user: newUser });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal Server Error' });
-  }
-});
+// router.post('/create', async (req, res) => {
+//   try {
+//     const newUser = new User(req.query);
+//     await newUser.save();
+//     res.status(201).json({ message: 'User created successfully', user: newUser });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: 'Internal Server Error' });
+//   }
+// });
 
 router.put('/edit/:userId', async (req, res) => {
   const userId = req.params.userId;
