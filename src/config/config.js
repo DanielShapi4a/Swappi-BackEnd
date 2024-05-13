964a82274433fc06f1ed097a91685f964b8c37ea
Daniel Shapira <danielshapira3@gmail.com>
2024-05-13 11:47:59 +0300

    <fix> security fixes

--- src/config/config.js
+++ src/config/config.js
@@ -2,16 +2,7 @@ require("dotenv").config();
 
 const config = {
   PORT: process.env.PORT || 5000,
-  DB_CONNECTION: `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@swappi.1g75bvc.mongodb.net/Swappi`,
-//   SECRET: "zsocstmp",
-
-//   SALT: 10,
-//   COOKIE_NAME: "USER_SESSION",
-
-//   CLOUDINARY_STORAGE: process.env.CLOUDINARY_STORAGE,
-//   CLOUDINARY_NAME: process.env.CLOUDINARY_NAME,
-//   CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
-//   CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+  DB_CONNECTION: `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASSWORD}@${process.env.DB_URL}`,
 };
 
 module.exports = config;
